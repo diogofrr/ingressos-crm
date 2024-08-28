66d9c3e0ab2562539ec706142b2402e8e660b577
Diogo Henrique Ferreira <diogo.ferreira@unitri.edu.br>
2024-08-28 16:15:03 -0300

    fix: correção no download de PDF no IOS

--- src/utils/handleDownloadPDF.ts
+++ src/utils/handleDownloadPDF.ts
@@ -1,6 +1,26 @@
 export const handleDownloadPdf = (hash: string) => {
-  const link = document.createElement("a");
-  link.href = hash;
-  link.download = `ingresso-crm.pdf`;
+  const base64WithoutPrefix = hash.substr('data:application/pdf;base64,'.length)
+
+  const bytes = atob(base64WithoutPrefix)
+  let length = bytes.length
+  const out = new Uint8Array(length)
+
+  while (length--) {
+    out[length] = bytes.charCodeAt(length)
+  }
+
+  const blob = new Blob([out], { type: 'application/pdf' })
+
+  // Cria um link para download
+  const link = document.createElement('a');
+  const url = URL.createObjectURL(blob);
+  
+  link.href = url;
+  link.download = 'ingresso-crm.pdf';
+  
+  // Simula o clique no link para iniciar o download
   link.click();
+  
+  // Limpa o objeto URL para liberar memória
+  URL.revokeObjectURL(url);
 }
